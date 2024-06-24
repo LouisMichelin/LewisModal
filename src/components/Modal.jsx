731cef2736038a46660e8a4302051feb38f9e19e
LouisMichelin <louis.michelin097@gmail.com>
2024-06-24 16:41:13 +0200

    Initialisation of LewisModal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,24 @@
+import "./Modal.scss";
+
+export function openModal() {
+   document.getElementById("validationModal").style.display = "block";
+}
+export function closeModal() {
+   document.getElementById("validationModal").style.display = "none";
+}
+
+// EDIT DEPUIS JS LE CSS
+
+function Modal() {
+   return (
+      <>
+         <div id="validationModal">
+            <div id="positionModal">
+               <button id="closeValidationModal" onClick={closeModal}></button>
+               <div id="confirmationModal">Employee Created !</div>
+            </div>
+         </div>
+      </>
+   );
+}
+export default Modal;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Modal from "./components/Modal.jsx";
+import "./index.scss";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+   <React.StrictMode>
+      <Modal />
+   </React.StrictMode>
+);
