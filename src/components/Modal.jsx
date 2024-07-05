dc98704a260506a62e47fb502afeeddbd345a29f
LouisMichelin <louis.michelin097@gmail.com>
2024-07-05 21:49:32 +0200

    Improvements & Edits for Props : Message, Bg, FontC, FontSize are now possible

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,12 +7,30 @@ export function closeModal() {
    document.getElementById("validationModal").style.display = "none";
 }
 
-export default function Modal() {
+export default function Modal({
+   modalMessage,
+   modalBackground,
+   modalFontColor,
+   modalFontSize,
+}) {
    return (
-      <div id="validationModal">
+      <div
+         id="validationModal"
+         style={{
+            backgroundColor: modalBackground,
+         }}
+      >
          <div id="positionModal">
             <button id="closeValidationModal" onClick={closeModal}></button>
-            <div id="confirmationModal">Employee Created !</div>
+            <div
+               id="confirmationModal"
+               style={{
+                  color: modalFontColor,
+                  fontSize: modalFontSize,
+               }}
+            >
+               {modalMessage}
+            </div>
          </div>
       </div>
    );
