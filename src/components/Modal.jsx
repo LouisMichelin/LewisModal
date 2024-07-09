74d3702602572b6c3060c4fd193e7fd41a83581e
LouisMichelin <louis.michelin097@gmail.com>
2024-07-09 18:15:45 +0200

    Added : Main div's background customizable RGBA - each of them editable

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -16,28 +16,40 @@ export default function Modal({
    modalBackground,
    modalFontColor,
    modalFontSize,
+   modalWidth,
+   modalHeight,
+   mRed,
+   mGreen,
+   mBlue,
+   mOpacity,
 }) {
    /*
-    * Default Values of the Modal (if none defined) :
+    * Here are the Default Values of the Modal (if none defined) :
     * - modalMessage : Displayed message on <Modal/>
     * - modalBackground : Color of the background on <Modal/>
     * - modalFontColor : Color of the message on <Modal/>
     * - modalFontSize : Size of the message on <Modal/>
-    *
-    *
+    * - modalWidth : Width of the <Modal/>
+    * - modalHeight : Height of the <Modal/>
+    * - mRed : Quantity of Red for Main Background's RGBa()
+    * - mGreen : Quantity of Green for Main Background's RGBa()
+    * - mBlue : Quantity of Blue for Main Background's RGBa()
+    * - mOpacity : Opacity for Main Background's RGBa()
     */
    const defaultModal = {
-      message: modalMessage || "Success !",
-      backgroundColor: modalBackground || "lightgreen",
-      fontColor: modalFontColor || "black",
-      fontSize: modalFontSize || 32,
+      message: modalMessage || "Success !", // String message between ""
+      backgroundColor: modalBackground || "lightgreen", // Colors (any sort)
+      fontColor: modalFontColor || "black", // Colors (any sort)
+      fontSize: modalFontSize || 32, // Size in pixels (px)
+      widthOfModal: modalWidth || 66, // Values between [0-100] (%age)
+      heightOfModal: modalHeight || 66, // Values between [0-100] (%age)
+      amountOfRed: mRed || 50, // Values between [0-255] (RGB values)
+      amountOfGreen: mGreen || 200, // Values between [0-255] (RGB values)
+      amountOfBlue: mBlue || 50, // Values between [0-255] (RGB values)
+      mainBackgroundOpacity: mOpacity || 25, // Values between [0-100] (%age)
    };
 
-   // MORE EDITABLES :
-   // - DEFAULT FONT : font-family: Arial, Helvetica, sans-serif;
-   // - WIDTH
-   // - HEIGHT
-
+   // Close Modal when clicking anywhere else
    function otherCloseModal(event) {
       let validation = document.getElementById("validationModal");
       if (validation === event.target) {
@@ -50,10 +62,16 @@ export default function Modal({
          id="validationModal"
          onClick={(e) => otherCloseModal(e)}
          style={{
-            backgroundColor: defaultModal.backgroundColor,
+            backgroundColor: `rgba(${defaultModal.amountOfRed}, ${defaultModal.amountOfGreen}, ${defaultModal.amountOfBlue}, ${defaultModal.mainBackgroundOpacity}%)`,
          }}
       >
-         <div id="positionModal">
+         <div
+            id="contentModal"
+            style={{
+               width: `${defaultModal.widthOfModal}%`,
+               height: `${defaultModal.heightOfModal}%`,
+            }}
+         >
             <button id="closeValidationModal" onClick={closeModal}></button>
             <div
                id="confirmationModal"
